809c89cca27757313a87f6e263a6ad53248feb81
Эльнара Кучина <elnara.kuchina@yandex.ru>
2024-01-12 13:10:57 +0400

    add secrets, hide nginx version

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+DEBUG = os.getenv('DEBUG')
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost')
 
 # Application definition
 
